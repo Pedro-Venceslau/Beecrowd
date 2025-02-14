6df2593ecb71890f48268818eec3e52a97d20de1
Pedro-Venceslau <pedro_venceslau97@hotmail.com>
2025-02-14 09:06:21 -0300

    1007.c

---
+++ 1007.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+    int A, B, C, D, difer;
+    
+    scanf("%A%B%C%D", &A, &B, &C, &D);
+    difer = ((A*B) - (C*D));
+    
+    printf("DIFERENCA = %d\n", difer);
+    
+    return 0;
+}
